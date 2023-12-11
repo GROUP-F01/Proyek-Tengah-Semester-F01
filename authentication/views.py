fa9724327d917584a4a00e8e9730379bcf5d0798
Scarletra <fernandovalentino10@gmail.com>
2023-12-11 15:43:32 +0700

    feat: register account

--- authentication/views.py
+++ authentication/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
 
 @csrf_exempt
 def login(request):
@@ -46,3 +47,17 @@ def logout(request):
         "status": False,
         "message": "Logout gagal."
         }, status=401)
+    
+
+@csrf_exempt
+def register(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    if User.objects.filter(username=username).exists():
+        return JsonResponse({"status": False, "message": "Register gagal, username sudah digunakan."}, status=400)
+
+    user = User.objects.create_user(username=username, password=password)
+    user.save()
+
+    return JsonResponse({"username": user.username, "status": True, "message": "Register berhasil!"}, status=201)
--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-<<<<<<< HEAD
 from main.views import show_main, create_product, register, login_user, logout_user
 from main.views import show_json
-=======
 from main.views import show_main, create_product, register, login_user, logout_user, show_buku
->>>>>>> 713a4845c6c72b5bf288cb580ff3f68ecd5a7cc3
 
 app_name = 'main'
 
@@ -15,9 +12,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),   
-<<<<<<< HEAD
     path('json/', show_json, name='show_json'),
-=======
     path('json/', show_buku, name='show_buku')
->>>>>>> 713a4845c6c72b5bf288cb580ff3f68ecd5a7cc3
 ]
